f6417c27c6534d04787eafa52f9a91b51184147c
walidarba <walidfiguig16@gmail.com>
2021-05-12 05:22:18 +0000

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
 
 app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
 
-@app.get("/", include_in_schema=False)
-async def index():
-    return RedirectResponse(url="/docs")
+#@app.get("/", include_in_schema=False)
+#async def index():
+#    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
 
-print("walid figuig")
+#print("walid figuig")
